2cfc3539b92ad7b8ffea1689a8bae3ba39f17953
samuelbroscheit <samuel.broscheit@gmail.com>
2020-06-14 06:13:41 +0200

    fix glob

--- bert_entity/preprocessing/collect_mention_entity_counts.py
+++ bert_entity/preprocessing/collect_mention_entity_counts.py
@@ -48,7 +48,7 @@ class CollectMentionEntityCounts(PipelineJob):
 
         workers = list()
 
-        list_dir_string = f"data/versions/{self.opts.data_version_name}/wikiextractor_out/{self.opts.wiki_lang_version}/{self.opts.data_version_name}*pages-articles*/*/wiki_*"
+        list_dir_string = f"data/versions/{self.opts.data_version_name}/wikiextractor_out/{self.opts.wiki_lang_version}/{self.opts.wiki_lang_version}*pages-articles*/*/wiki_*"
 
         #
         # start the workers in individual processes
